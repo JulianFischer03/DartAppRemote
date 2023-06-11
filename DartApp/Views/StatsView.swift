623720b1e42cd122205f5ecff7fd7c05fd7ded4f
Julian Fischer <Julian3.Fischer@Gmail.com>
2023-06-11 19:44:12 +0200

    Diagramm für DartsProLeg eingeführt. Achsenbeschriftung noch fehlerhaft

--- DartApp/Views/StatsView.swift
+++ DartApp/Views/StatsView.swift
@@ -12,6 +12,19 @@ struct StatsView: View {
     
     @EnvironmentObject private var dataManager: DataManager
     
+    struct dartsProLeg {
+        var legNummer: Int
+        var dartsProLeg: Int
+    }
+
+    var data: [dartsProLeg] {
+            var dataArray: [dartsProLeg] = []
+            for (index, darts) in dataManager.stats.dartsProLeg.enumerated() {
+                let item = dartsProLeg(legNummer: index, dartsProLeg: darts)
+                dataArray.append(item)
+            }
+            return dataArray
+        }
    
     
     var body: some View {
@@ -20,65 +33,58 @@ struct StatsView: View {
             
             Color.black
             
-            RoundedRectangle(cornerRadius: 30, style: .continuous)
+        /*    RoundedRectangle(cornerRadius: 30, style: .continuous)
                 .foregroundStyle(.linearGradient(colors: [.pink, .red],startPoint:.topLeading, endPoint: .bottomTrailing))
                 .frame(width: 1000, height: 400)
                 .rotationEffect (.degrees (225))
                 .offset (y: -350)
-            
+            */
                 
-                VStack {
-                    Text("Hello, Stats!")
-                        .font(.title)
-                        .foregroundColor(.white)
-                        .bold()
-                        .padding(10)
-                        .background(
-                            RoundedRectangle(cornerRadius: 6, style: .continuous)
-                                .fill(Color.green.shadow(.drop(radius: 2)))
+            VStack {
+                Text("Hello, Stats!")
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .bold()
+                    .padding(25)
+                
+                
+                Text("Deine geworfenen Darts: \(dataManager.stats.darts)")
+                    .font(.body)
+                    .foregroundColor(.white)
+                    .bold()
+                    .padding(10)
+                
+                Text("Deine geworfenen 180s: \(dataManager.stats.hundertAchtziger)")
+                    .font(.body)
+                    .foregroundColor(.white)
+                    .bold()
+                    .padding(10)
+                
+                Text(String(format: "Deine gesamte Doppelquote: %.2f", dataManager.stats.doppelQuote))
+                    .font(.body)
+                    .foregroundColor(.white)
+                    .bold()
+                    .padding(10)
+                
+                HStack{
+                    Chart(data, id: \.legNummer) { item in
+                        BarMark(
+                            x: .value("Leg", item.legNummer),
+                            y: .value("darts", item.dartsProLeg)
                         )
-                        .padding(25)
-                    
-                    
-                    Text("Deine geworfenen Darts: \(dataManager.stats.darts)")
-                        .font(.body)
-                        .foregroundColor(.white)
-                        .bold()
-                        .padding(10)
-                        .background(
-                            RoundedRectangle(cornerRadius: 6, style: .continuous)
-                                .fill(Color.green.shadow(.drop(radius: 2)))
-                    )
-                    .padding(15)
-                    
-                    Text("Deine geworfenen 180s: \(dataManager.stats.hundertAchtziger)")
-                        .font(.body)
-                        .foregroundColor(.white)
-                        .bold()
-                        .padding(10)
-                        .background(
-                            RoundedRectangle(cornerRadius: 6, style: .continuous)
-                                .fill(Color.green.shadow(.drop(radius: 2)))
-                    )
-                    .padding(15)
-                    
-                    Text(String(format: "Deine gesamte Doppelquote: %.2f", dataManager.stats.doppelQuote))
-                        .font(.body)
-                        .foregroundColor(.white)
-                        .bold()
-                        .padding(10)
-                        .background(
-                            RoundedRectangle(cornerRadius: 6, style: .continuous)
-                                .fill(Color.green.shadow(.drop(radius: 2)))
-                    )
-                    .padding(15)
-                    
-                    
-                    
+                    }
+                    .cornerRadius(20)
+                    .frame(maxWidth: 300, maxHeight: 300)
+                    .foregroundStyle(Color.green.gradient)
+                   // .background(.gray)
                 }
+                .padding()
+            }
+            
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
             }
     }
+    
 }
 
 struct StatsView_Previews: PreviewProvider {
